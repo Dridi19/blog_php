9e120d44b6e7bbd2aa0f2c88c8a28ed560c948e2
DESKTOP-J0IT8EV\Jean <jcdfabel.externe@g7.fr>
2022-11-28 02:13:03 +0100

    cc

---
+++ app/views/users.php
@@ -0,0 +1,13 @@
+<table>
+    <thead>
+        <th>first name</th>
+        <th>last name</th>
+        <th>email</th>
+        <th>is admin</th>
+        <th>action</th>
+    </thead>
+
+    <tbody>
+        <td><php $irstname></td>
+    </tbody>
+</table>
